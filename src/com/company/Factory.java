9c43c6f77ae91a7c78d8adb8d1dec66a5871a8c0
Valeria297 <valeriserbin2907@gmail.com>
2021-10-25 23:39:38 +0300

    Homework_6

--- src/com/company/Factory.java
+++ src/com/company/Factory.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Factory extends Thread {
+public class Factory {
 
     private Robot robot = new Robot();
 
